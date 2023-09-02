462d18e476cbb70eb32454c0c5980cd464429e49
han sheng <gh890528@gmail.com>
2023-09-02 22:36:58 +0800

    finish shop

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const express = require('express')
 import express from 'express'
 import mysql from 'mysql2'
+import cors from 'cors'
 
 const app = express()
 const db = mysql.createConnection({
@@ -11,6 +12,7 @@ const db = mysql.createConnection({
 })
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/" , (req, res)=>{
     res.json('shop server running')
@@ -25,9 +27,35 @@ app.get("/commobity", (req, res) => {
 })
 
 app.post("/commobity" , (req, res) => {
-    const q = "INSERT INTO commpbity (`title`, `price`, `number`) VALUE (?)"
-    const value = []
+    const q = "INSERT INTO commobity (`title`, `price`, `number`) VALUE (?)"
+    const value = [
+        req.body.title,
+        req.body.price,
+        req.body.number
+    ]
     db.query(q, [value], (err, data) => {
+        if(err) return console.log(err)
+        return res.json(data)
+    })
+})
+
+app.delete("/commobity/:id", (req, res) => {
+    const commobityId = req.params.id
+    const q = "DELETE FROM commobity WHERE id = ?"
+    db.query(q, [commobityId], (err, data) => {
+        if(err) return console.log(err)
+        return res.json(data)
+    })
+})
+app.put("/commobity/:id" , (req, res) => {
+    const commobityId = req.params.id
+    const q = "UPDATE commobity SET `title` = ?, `price` = ?, `number` = ? WHERE id = ?"
+    const value = [
+        req.body.title,
+        req.body.price,
+        req.body.number
+    ]
+    db.query(q, [...value, commobityId], (err, data) => {
         if(err) return console.log(err)
         return res.json(data)
     })
